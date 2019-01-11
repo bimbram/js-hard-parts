a85ab541ca7146d210760a216d918d1f6f41d969
Bimo Bramantyo <bim.bramantyo@gmail.com>
2019-01-11 14:52:14 +0700

    finished section 1 and section 2

---
+++ 02-principle-of-js/001_global_execution_context/slide_02.js
@@ -0,0 +1,8 @@
+// What happens when JavaScript executes (runs) my code
+
+const num = 3;
+function multiplyBy2(inputNumber) {
+  const result = inputNumber * 2;
+  return result;
+}
+const name = "Will";
